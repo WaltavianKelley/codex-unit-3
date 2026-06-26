9b896d794294c8c0ec1c6df729f40c2644050cd9
Waltavian Kelley <a.Babycane421@outlook.fr>
2026-06-26 16:12:53 -0500

    levels 5-10 complete

---
+++ lesson-02-npm/level-10-gitignore/script.js
@@ -0,0 +1,6 @@
+import terminalLink from 'terminal-link';
+
+const link = terminalLink('My Website', 'https://sindresorhus.com');
+console.log(link);
+
+console.log("Gitignore is the file that helps you not push sensitive or unwanted data to your repository.")
